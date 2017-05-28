6c5e01f229f6eaf445f08e277ad2c672c0d6a107
alljp <allenjosesmailbox@gmail.com>
2017-05-28 22:12:42 +0530

    Add Env function to check for keyword in env.

--- env.js
+++ env.js
@@ -1,5 +1,24 @@
 const op = require("./operators");
 console.log(op);
+var Env = function(d) {
+    var env = {};
+    var outer = d.outer || [];
+    if (d.parms.length != 0) {
+        for (var i = 0; i < d.parms.length; i += 1) {
+            env[d.parms[i]] = d.args[i];
+        }
+    }
+    env.find = function(var_) {
+        if (env.hasOwnProperty(var_)) {
+            return env;
+        } else {
+            return outer.find(var_)
+        }
+    }
+    return env;
+}
+
+
 
 const add_globals = function(env) {
     console.log("\nadd_globals\n");
@@ -8,10 +27,13 @@ const add_globals = function(env) {
         env[key] = op[key];
     }
     console.log(env);
+    // console.log(op);
+
     console.log("add_globals: env: ", env);
     return env;
 }
 
 module.exports = {
+    Env: Env,
     add_globals: add_globals
 };

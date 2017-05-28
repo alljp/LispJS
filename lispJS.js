15ae16018dbe3262c8301c22176e416d670575b1
alljp <allenjosesmailbox@gmail.com>
2017-05-28 22:43:57 +0530

    Requires parse() and tokenize() from parser.js and eval() from eval.js to implement the interpreter. Modified the prompt() method to act as a REPL

--- lispJS.js
+++ lispJS.js
@@ -1,52 +1,25 @@
+const parse = require("./parser").parse;
+const tokenize = require("./parser").tokenize;
 const eval = require("./eval");
-const parser = require("./parser").parser;
-const tokenize = require("./parser").tokenize
 
-let prompt = function(question, callback) {
-    const stdin = process.stdin,
-        stdout = process.stdout;
 
+var repl = function() {
+    var stdin = process.stdin;
+    var stdout = process.stdout;
     stdin.resume();
-    stdout.write(question);
-
-    stdin.once('data', function(data) {
-        callback(data.toString().trim());
-    });
+    stdout.write(">>>");
+    stdin.on('data', function(data) {
+        data = data.toString().trim();
+        if (data == undefined) {
+            stdin.write('>>>');
+        }
+        var result = eval(parse(tokenize(data)));
+        if (result != undefined) {
+            stdout.write(result + '\n>>>');
+        } else {
+            stdout.write('>>>');
+        }
+    })
 }
 
-
-
-// let paranthesize = function(input, list = []) {
-//     let token = input.shift();
-//     if (token == undefined) {
-//         return list.pop();
-//     } else if (token == "(") {
-//         list.push(paranthesize(input, []));
-//         return paranthesize(input, list);
-//     } else if (token == ")") {
-//         return list;
-//     } else {
-//         return paranthesize(input, list.concat(categorize(token)));
-//     }
-// };
-
-// let categorize = function(input) {
-//     if (!isNaN(parseFloat(input))) {
-//         return { type: 'literal', value: parseFloat(input) };
-//     } else if (input[0] == '"' & input.slice(-1) == '"') {
-//         return { type: 'literal', value: input.slice(1, -1) };
-//     } else {
-//         return { type: 'symbol', value: input };
-//     }
-// };
-
-
-
-
-prompt(">>",
-    function(input) {
-        let temp = parser(tokenize(input));
-        console.log(temp);
-        console.log(eval(temp));
-        process.exit();
-    });
+repl();
